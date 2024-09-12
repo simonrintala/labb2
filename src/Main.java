e0e566d781cbbdb9b7a7ac911fc97f5d078fab94
Simon Rintala <siimon_7@icloud.com>
2024-09-12 10:50:47 +0200

    Ingredienser tillagt

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Kycklinggryta med curry");
+        System.out.println("Ingredienser:");
+        System.out.println("700 g kycklingfilé\n" +
+                "2 röda paprikor\n" +
+                "2 gula lökar\n" +
+                "2 msk smör, att steka i\n" +
+                "ca 3 msk curry\n" +
+                "1 msk paprikapulver\n" +
+                "5 dl vispgrädde\n" +
+                "2 dl crème fraîche\n" +
+                "2 msk koncentrerad apelsinjuice\n" +
+                "2 msk kinesisk soja\n" +
+                "2 krm svartpeppar");
     }
 }
