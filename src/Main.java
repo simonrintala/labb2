e1054c973299bcc9aacec5b2d4e47a3241c34a6c
unknown <abdullah.hussein@studerande.plushogskolan.se>
2024-09-12 12:07:58 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kycklinggryta med curry");
+        System.out.println("Krispig torsk i ugn");
         System.out.println("Ingredienser:");
-        System.out.println("700 g kycklingfilé\n" +
-                "2 röda paprikor\n" +
-                "2 gula lökar\n" +
-                "2 msk smör, att steka i\n" +
-                "ca 3 msk curry\n" +
-                "1 msk paprikapulver\n" +
-                "5 dl vispgrädde\n" +
-                "2 dl crème fraîche\n" +
-                "2 msk koncentrerad apelsinjuice\n" +
-                "2 msk kinesisk soja\n" +
-                "2 krm svartpeppar");
+        System.out.println("6 torskfiléer (tinade eller färska portionsbitar) [ca 750 g]\n" +
+                "1,5 tsk salt\n" +
+                "0,5 tsk malen svartpeppar\n" +
+                "0,5 msk citronskal, finrivet\n" +
+                "1 msk citronsaft, färskpressad\n" +
+                "1 dl olivolja\n" +
+                "1 msk färsk dragon, hackad\n" +
+                "1 dl persilja, hackad\n" +
+                "4 dl vitt bröd, finhackat (gärna surdeg)\n" +
+                "2 dl lagrad ost, riven (t ex västerbottensost)\n" +
+                "300 g körsbärstomater (på kvist)\n");
+        System.out.println("Till servering\n" +
+                "potatis, kokt och pressad\n" +
+                "sallad\n");
+        System.out.println("instruktion");
+        System.out.println("1.Värm ugnen till 175 grader, varmluft.\n" +
+                "Lägg fisken i en smord ugnsform och gnid in den runt om med salt,\n" +
+                "peppar, citronskal och juice.\n" +
+                "2.\n" +
+                "3.Blanda olja, dragon, persilja, bröd och ost i en skål.\n" +
+                "4.Fördela blandningen över fiskbitarna i formen.\n" +
+                "5.Lägg körsbärstomaterna hela bredvid.\n" +
+                "Tillaga mitt i ugnen i 15–25 minuter eller tills ytan är gyllene och\n" +
+                "fisken är klar i mitten. Om du har en termometer ska\n" +
+                "innertemperaturen vara cirka 52 grader.\n" +
+                "6.\n" +
+                "7.Servera med pressad potatis och gärna en grönsallad");
+
+
     }
 }
