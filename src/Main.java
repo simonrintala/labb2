417dcecf06cba41e0daa7df56bebba0d4dac1472
Simon Rintala <siimon_7@icloud.com>
2024-09-12 11:32:34 +0200

    Added instructions

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,15 @@ public class Main {
                 "2 msk koncentrerad apelsinjuice\n" +
                 "2 msk kinesisk soja\n" +
                 "2 krm svartpeppar");
+        System.out.println("Instruktion:\n" +
+                "1. Skär kycklingfiléerna i bitar.\n" +
+                "2. Skölj, kärna ur och skär paprikan i bitar.\n" +
+                "3. Skala och skär löken i klyftor.\n" +
+                "4. Bryn kycklingen i smör i en stor gryta.\n" +
+                "5. Tillsätt paprika, lök, curry och paprikapulver och låt fräsa cirka 3\n" +
+                "minuter.\n" +
+                "6. Tillsätt vispgrädde, crème fraiche, apelsinjuice, soja och peppar. Låt\n" +
+                "bli varmt och se till att kycklingen är genomstekt. Servera!");
+
     }
 }
