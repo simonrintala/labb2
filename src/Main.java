52abc43dbef016cbe6b842a2642194d36ffa7f09
Teo Lind <teo.l.lind@gmail.com>
2024-09-12 11:28:29 +0200

    added instructions

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,18 @@ public class Main {
                 " nymalen svartpeppar\n" +
                 " 1 förp Zeta Tagliatelle Casa Di Luca\n" +
                 " Zeta Grana Padano 16 mån, nyriven");
+
+        System.out.println("\nInstruktioner:"
+                +" Instruktion:\n" +
+                " 1.Stek nötfärs på hög värme i olivolja i en gryta tills färsen är\n" +
+                " genomstekt.\n" +
+                " 2.Tillsätt lök, vitlök, morot och selleri.\n" +
+                " 3.Stek ytterligare 1 minut.\n" +
+                " 4.Lägg i tomatpuré, vitt vin och krossade tomater.\n" +
+                " 5.Låt koka upp och lägg i buljongtärning och örter, låt puttra ca 45 minuter under lock.\n" +
+                " 6.Rör regelbundet och späd eventuellt med lite vatten.\n" +
+                " 7.Smaka av med salt och peppar och servera med nykokt pasta och\n" +
+                " Grana Padano");
+
     }
 }
