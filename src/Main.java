a863e47059e56690e83e4d96c0dad4b8ba740b4f
Teo Lind <teo.l.lind@gmail.com>
2024-09-12 11:24:22 +0200

    added list of ingredients

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bolognese med Tagliatelle");
-        System.out.println("Ingredienser:");
+        System.out.println("\nIngredienser:"
+        +"\n400 g nötfärs\n" +
+                " 4 msk Zeta extra jungfruolivolja classico\n" +
+                " 1 gul lök, finhackad\n" +
+                " 2 klyftor vitlök, finhackade\n" +
+                " 1 morot, finhackad\n" +
+                " 1 stjälk stjälkselleri, finhackad\n" +
+                " 1 dl Zeta Matlagningsvin Vitt\n" +
+                " 1 förp krossade tomater\n" +
+                " 2 msk Zeta Tomatpuré\n" +
+                " 1 köttbuljongtärning\n" +
+                " 2 lagerblad\n" +
+                " 1 tsk torkad timjan\n" +
+                " 1 tsk torkad oregano\n" +
+                " salt\n" +
+                " nymalen svartpeppar\n" +
+                " 1 förp Zeta Tagliatelle Casa Di Luca\n" +
+                " Zeta Grana Padano 16 mån, nyriven");
     }
 }
